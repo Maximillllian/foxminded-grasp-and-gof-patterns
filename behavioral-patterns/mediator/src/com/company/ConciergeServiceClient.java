9154ba121f1acc48225334afc4da0b1553ddb8f5
mikhail-z <mike.rzn@yandex.ru>
2020-08-16 11:23:50 +0300

    реализован шаблон посредник

---
+++ behavioral-patterns/mediator/src/com/company/ConciergeServiceClient.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class ConciergeServiceClient{
+    private String name;
+
+    public void notify(String event) {
+        System.out.println("client received: " + event);
+    }
+
+    public ConciergeServiceClient(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    private void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ behavioral-patterns/mediator/src/com/company/IMediator.java
@@ -0,0 +1,5 @@
+package com.company;
+
+public interface IMediator {
+    void send(Object sender, String event);
+}
