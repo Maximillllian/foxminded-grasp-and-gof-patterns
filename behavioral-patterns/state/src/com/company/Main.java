b5e7ecaa9d127d156cea48c1f496a4d60adb799a
mikhail-z <mike.rzn@yandex.ru>
2020-08-24 21:50:20 +0300

    Реализован шаблон состояние

---
+++ behavioral-patterns/state/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        IOven oven = new Oven(25);
+        for (int i = 0; i < 8; i++) {
+            oven.increaseTemperature();
+            try {
+                oven.bake();
+                return;
+            } catch (Exception ex) {
+                System.out.println("can't bake when " + oven.getTemperature() + "C");
+            }
+        }
+    }
+}
