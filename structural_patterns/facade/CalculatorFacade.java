e53c089ad2fda4513c4174fe75cbcd812db6a249
Maxim Woronin <voronin.maksim@mail.com>
2023-12-12 19:04:57 +0500

    [improvement] Вынесен интерфейс CalculatorFacade

--- structural_patterns/facade/CalculatorFacade.java
+++ structural_patterns/facade/CalculatorFacade.java
@@ -1,41 +1,11 @@
 package structural_patterns.facade;
 
-import jakarta.xml.soap.*;
-import structural_patterns.facade.calculator.org.tempuri.*;
-import structural_patterns.facade.soap.CalculatorSOAP;
+public interface CalculatorFacade {
+    int add(int number1, int number2);
 
-public class CalculatorFacade {
-    private final static CalculatorSOAP soap = new CalculatorSOAP();
+    int multiply(int number1, int number2);
 
-    public static int add(int number1, int number2) {
-        var add = new Add();
-        add.setIntA(number1);
-        add.setIntB(number2);
-        var response = soap.sendMessageToSOAPServer(add, "Add", AddResponse.class);
-        return response.getAddResult();
-    }
+    int divide(int number1, int number2);
 
-    public static int multiply(int number1, int number2) {
-        var multiply = new Multiply();
-        multiply.setIntA(number1);
-        multiply.setIntB(number2);
-        var response = soap.sendMessageToSOAPServer(multiply, "Multiply", MultiplyResponse.class);
-        return response.getMultiplyResult();
-    }
-
-    public static int divide(int number1, int number2) {
-        var divide = new Divide();
-        divide.setIntA(number1);
-        divide.setIntB(number2);
-        var response = soap.sendMessageToSOAPServer(divide, "Divide", DivideResponse.class);
-        return response.getDivideResult();
-    }
-
-    public static int subtract(int number1, int number2) {
-        var subtract = new Subtract();
-        subtract.setIntA(number1);
-        subtract.setIntB(number2);
-        var response = soap.sendMessageToSOAPServer(subtract, "Subtract", SubtractResponse.class);
-        return response.getSubtractResult();
-    }
+    int subtract(int number1, int number2);
 }
--- structural_patterns/facade/Main.java
+++ structural_patterns/facade/Main.java
@@ -2,13 +2,14 @@ package structural_patterns.facade;
 
 public class Main {
     public static void main(String[] args) {
-        var addResult = CalculatorFacade.add(1, 2);
+        var calculator = new CalculatorFacadeImpl();
+        var addResult = calculator.add(1, 2);
         System.out.println("AddResult - " + addResult);
-        var subtractResult = CalculatorFacade.subtract(4, 2);
+        var subtractResult = calculator.subtract(4, 2);
         System.out.println("subtractResult - " + subtractResult);
-        var divideResult = CalculatorFacade.divide(18, 3);
+        var divideResult = calculator.divide(18, 3);
         System.out.println("divideResult - " + divideResult);
-        var multiplyResult = CalculatorFacade.multiply(5, 2);
+        var multiplyResult = calculator.multiply(5, 2);
         System.out.println("multiplyResult - " + multiplyResult);
     }
 }
